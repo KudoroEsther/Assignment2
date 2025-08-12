787f7f775fe6f9ac000cb29d89b276fc522ca8dd
Kudoro Esther <kudoroesther.learn@gmail.com>
2025-08-12 16:00:12 +0100

    Edited some tasks

--- Task_4.py
+++ Task_4.py
@@ -4,25 +4,32 @@
 # quotes = quote.title()
 # print(f'\"{quotes}\"')
 
-#TASK 2 Shopping list master
-empty = []
-items = input("Enter 3 shopping items one by one with commas in between: ").split()
-empty = items.copy()
-print(empty)
+# #TASK 2 Shopping list master
+# empty = []
+# items = input("Enter 3 shopping items one by one with commas in between: ").split()
+# empty = items.copy()
+# print(empty)
 
 # #Task 3 Word Counter
 # sent = input("Enter a sentence: ")
 # sent_list = list(sent.split()) #this converts the inputed sentence to list
 # print(len(sent_list))
 
-# #Task 4 Name Organizer
-# names = []
-# for i in range(2):
-#     name = list(input("Input 5 names: ").lower())
-#     names.append(name)
-#     i = i+1
-    
-# print(name)
+# # #Task 4 Name Organizer
+# names = input("Enter five names separated by spaces: ").split()
+# names = [name.lower() for name in names]
+# print(*names ,sep="\n")
+
+#OR
+name = input("Enter five names separated by spaces: ").lower().split()
+name.sort()
+print(*name, sep="\n")
 
 # #Task 5 Student Score Tracker
-# names = input("Input three names: ")
+# list_name = []
+# list_score = []
+
+# name = input("Input three names: ").split()
+# score = int(input("Enter the score for each student: "))
+# list_name =name.copy()
+# print(list_name)
